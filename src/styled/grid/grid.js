de44bd1afeb04bbef2f2cc92a9a4b5e9921f17af
Daniel <dcastillo3@gmail.com>
2019-07-11 16:48:47 -0400

    Chore(grid): Build grid component and adjust background color

--- src/styled/grid/grid.js
+++ src/styled/grid/grid.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import styled from 'styled-components';
+import Box from '../box/box';
 
-const Grid = props => {
-    return (
-        <div>{props.children}</div>
-    )
-}
+const Grid = styled(Box)`
+    ${props => props.justifyContent? `justify-content: ${props.justifyContent}` : ''};
+    ${props => props.wrap? `flex-wrap: ${props.wrap}` : ''};
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+`;
+
+export const GridColumn = styled(Grid)`
+    align-items: ${props => props.alignItems ? props.alignItems : 'flex-start'};
+    flex-direction: column;
+`;
 
 export default Grid;
