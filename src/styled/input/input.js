b89daa0e82e23c1f06d27f9da40326c695f0613e
Daniel Castillo <dcastillo3@gmail.com>
2019-07-10 21:04:30 -0400

    Chore(input): Build inpu component and refactor existing imports

--- src/styled/input/input.js
+++ src/styled/input/input.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Input = props => {
-    const { inputValue, inputChange } = props;
-
-    return (
-        <input type="text" value={inputValue} onChange={inputChange} />
-    )
-}
+const Input = styled.input`
+    padding-right: 48px;
+    box-shadow: 0 0 0px 5px rgba(20,28,34,0.1);
+    box-sizing: border-box;
+    width: 100%;
+    font-size: 18px;
+    padding: 10px;
+    background: #fff;
+    border: none;
+    border-radius: 5px;
+    font-weight: 300;
+    color: #141c22;
+    outline: none;
+`;
 
 export default Input;
